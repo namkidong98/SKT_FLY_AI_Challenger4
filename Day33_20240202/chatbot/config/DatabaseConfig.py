e8244bca68d9d3eef487218eaa84c13c21dd4f4d
Kidong Nam <113520117+namkidong98@users.noreply.github.com>
2024-03-02 17:25:43 +0900

    Add files via upload

---
+++ Day33_20240202/chatbot/config/DatabaseConfig.py
@@ -0,0 +1,7 @@
+DB_HOST = "172.23.254.247" # ipconfig로 본인 무선 LAN의 IP로 설정
+DB_USER = "root"
+DB_PASSWORD = "apptools"
+DB_NAME = "flyai"
+
+def DatabaseConfig():
+    global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
